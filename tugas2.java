adc87f916667fc86f454d10c719dab8c87b9fe65
Your Name <you@example.com>
2024-09-12 11:41:51 +0700

    tugas ke 2

---
+++ tugas2.java
@@ -0,0 +1,23 @@
+import java.util.Scanner;
+
+public class tugas2 {
+    public static void main (String[] args) {
+
+        Scanner sc = new Scanner(System.in);
+
+        int jmlJam;
+        double gajiPerJam = 100000;
+
+        System.out.print("Masukkan jam kerja: ");
+        jmlJam = sc.nextInt();
+
+        double totalGaji = (jmlJam * gajiPerJam);
+        double gajiSblmPajak = totalGaji + ((totalGaji * 10)/100);
+        double gajiStlhPajak = (gajiSblmPajak * 95)/100;
+
+        System.out.println("Total Gaji : " + totalGaji);
+        System.out.println("Gaji Sebelum Pajak : " + gajiSblmPajak);
+        System.out.println("Gaji Setelah Pajak : " + gajiStlhPajak);
+
+    }
+}
